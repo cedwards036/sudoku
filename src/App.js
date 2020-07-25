826b0a28d599fe94905b548d7ee35eb6f7e48b93
Christopher Edwards <cedwards036@gmail.com>
2020-07-25 11:22:43 -0400

    Add a page header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
-        <Game/>
+      <Header/>
+      <Game/>
     </div>
   );
 }
