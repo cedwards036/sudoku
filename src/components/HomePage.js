4ad7c12e2b2506c779115c16d3d2f6c4d068eb44
Christopher Edwards <cedwards036@gmail.com>
2020-08-16 11:21:50 -0400

    update homepage with example puzzle links

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,10 +13,9 @@ export default function HomePage() {
                         Sudoku is a logic-based, combinatorial number-placement puzzle. 
                         In classic sudoku, the objective is to fill a 9×9 grid with digits 
                         so that each column, each row, and each of the nine 3×3 subgrids 
-                        that compose the grid (also called "boxes", "blocks", or "regions") 
-                        contain all of the digits from 1 to 9. The puzzle setter provides 
-                        a partially completed grid, which for a well-posed puzzle has a 
-                        single solution.
+                        that compose the grid...contain all of the digits from 1 to 9. 
+                        The puzzle setter provides a partially completed grid, which 
+                        for a well-posed puzzle has a single solution.
                     </q>&mdash;<a href="https://en.wikipedia.org/wiki/Sudoku" target="_blank" rel="noopener noreferrer">Wikipedia</a>
                 </p>
                 <p>
@@ -29,6 +28,11 @@ export default function HomePage() {
                     process as they solve puzzles in real time has taught me more about
                     solving Sudoku puzzles than all other resources combined.
                 </p>
+                <p className="footnote">
+                    *Cracking the Cryptic actually covers a wide range of puzzles,
+                    including many Sudoku variants and some non-Sudoku puzzles. This
+                    app, for the time being, is only concerned with "Classic" Sudoku.
+                </p>
                 <h2>How does this app work?</h2>
                 <p>
                     Click the button above to enter a puzzle into the app for solving.
@@ -39,22 +43,20 @@ export default function HomePage() {
                     Once you have entered a puzzle, click the "Solve" button to 
                     go to its Solve page. Per Cracking the Cryptic's recommendations,
                     this app allows for both corner pencil marks for box candidates 
-                    (Snyder notation) and center pencil marks for cell candidates. Other 
-                    features, such as automatic number highlighting and error indication,
-                    are optional and can be toggled on or off in the settings.
+                    (Snyder notation) and center pencil marks for cell candidates.
                 </p>
                 <p>
                     Because this app encodes puzzles as URLs, you can share a puzzle 
-                    simply by copying and sharing the solve link. 
-                    Just click the "Share" button to automatically copy the link to 
-                    your clipboard, or manually copy the URL in your browser's address
-                    bar.
+                    by copying and sharing the solve link. Just click the 
+                    "Share" button to automatically copy the link to your clipboard, 
+                    or manually copy the URL in your browser's address bar.
                 </p>
-                <p className="footnote">
-                    *Cracking the Cryptic actually covers a wide range of puzzles,
-                    including many Sudoku variants and some non-Sudoku puzzles. This
-                    app, for the time being, is only concerned with "Classic" Sudoku.
-                </p>
+                <h2>Try it out</h2>
+                <div className="example-puzzles">
+                    <Link to="/solve/93tZMuCwULWV7FWnFbmDyFTFVUDdSeNCjsiQbNPFSC0GI" target="_blank" rel="noopener noreferrer">Easy Puzzle</Link>
+                    <Link to="/solve/9SL1vPJO7vnBoivhWksTTqOSDPXXHPklYFJpwcvCgSSs2T" target="_blank" rel="noopener noreferrer">Medium Puzzle</Link>
+                    <Link to="/solve/916dCgTKpyzzmdA0z74k2LFdrFfn3h9UH46gauTd3Jr9kOw" target="_blank" rel="noopener noreferrer">Hard Puzzle</Link>
+                </div>
             </div>
         </div>
     )
